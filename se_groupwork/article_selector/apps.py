804a6bec91a485aef79073b94e1fc0dab26f4b6c
isXSong <wang-xs22@mails.tsinghua.edu.cn>
2025-11-12 00:14:21 +0800

    style: 统一命名风格，将信号机制应用于更新，调整优化了article_selector的tests

--- se_groupwork/article_selector/apps.py
+++ se_groupwork/article_selector/apps.py
@@ -3,4 +3,7 @@ from django.apps import AppConfig
 
 class ArticleselectorConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "articleSelector"
+    name = "article_selector"
+
+    def ready(self):
+        import article_selector.signals
