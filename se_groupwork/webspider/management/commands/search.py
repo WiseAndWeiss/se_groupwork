66d4df16dd975162a46d50b14d44570b66fb896b
WiseAndWeiss <lu-yt23@mails.tsinghua.edu.cn>
2025-12-04 17:21:34 +0800

    feat: 优化search指令，使其支持多公众号

--- se_groupwork/webspider/management/commands/search.py
+++ se_groupwork/webspider/management/commands/search.py
@@ -6,14 +6,17 @@ class Command(BaseCommand):
     help = '搜索公众号'
 
     def add_arguments(self, parser):
-        parser.add_argument('account_name', type=str, help='公众号名称')
+        parser.add_argument('account_names', nargs='+', type=str, help='一个或多个公众号名称')
 
     def handle(self, *args, **options):
-        account_name = options['account_name']
+        account_names = options['account_names']
 
-        try:
-            biz_searcher = BizSearcher(account_name)
-            biz_searcher.biz_search()  
-            self.stdout.write(self.style.SUCCESS(f'成功搜索到公众号"{account_name}"有关信息！'))
-        except Exception as e:
-            self.stdout.write(self.style.ERROR(f'搜索公众号"{account_name}"时出错: {str(e)}'))
+        for account_name in account_names:
+            self.stdout.write(f'正在搜索公众号: {account_name}')
+            try:
+                biz_searcher = BizSearcher(account_name)
+                biz_searcher.biz_search()  
+                self.stdout.write(self.style.SUCCESS(f'成功搜索到公众号"{account_name}"有关信息！'))
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(f'搜索公众号"{account_name}"时出错: {str(e)}'))
+        
